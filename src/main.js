9045c8109382159e9222f5bf41a5463fcaf9d7df
colbyfayock <fayock@gmail.com>
2021-10-25 22:37:16 -0400

    Step 3: using the AccuWeather API to add a forecast to the report email

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
+const fetch = require('node-fetch');
 
 (async function run() {
   console.log('Running report...');
+
+  const city = 'Philadelphia, PA';
+  const countryCode = 'US';
+
+  const locationEndpoint = `http://dataservice.accuweather.com/locations/v1/cities/${countryCode}/search`;
+  const locationRequest = await fetch(`${locationEndpoint}?q=${encodeURIComponent(city)}&apikey=${process.env.ACCUWEATHER_API_KEY}`);
+  const locationData = await locationRequest.json();
+  const locationKey = locationData[0].Key;
+
+  const forecastEndpoint = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`;
+  const forecastRequest = await fetch(`${forecastEndpoint}?apikey=${process.env.ACCUWEATHER_API_KEY}`);
+  const forecastData = await forecastRequest.json();
 
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -19,10 +32,19 @@ const nodemailer = require('nodemailer');
     to: process.env.DAILY_REPORT_TO,
     subject: 'Daily Report',
     text: `
-      Daily Report
+Daily Report
+
+Weather
+- Forecast: ${forecastData.Headline.Text}
+- Temp Min: ${forecastData.DailyForecasts[0].Temperature.Minimum.Value}° ${forecastData.DailyForecasts[0].Temperature.Minimum.Unit}
+- Temp Max: ${forecastData.DailyForecasts[0].Temperature.Maximum.Value}° ${forecastData.DailyForecasts[0].Temperature.Maximum.Unit}
     `,
     html: `
       <h1>Daily Report</h1>
+      <h2>Weather</h2>
+      <p>Forecast: ${forecastData.Headline.Text}</p>
+      <p>Temp Min: ${forecastData.DailyForecasts[0].Temperature.Minimum.Value}° ${forecastData.DailyForecasts[0].Temperature.Minimum.Unit}</p>
+      <p>Temp Max: ${forecastData.DailyForecasts[0].Temperature.Maximum.Value}° ${forecastData.DailyForecasts[0].Temperature.Maximum.Unit}</p>
     `,
   });
 
